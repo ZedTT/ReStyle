7bbf9b44a2bc6ce181039bc65f1c35aaee709572
ZedTT <ZackThomasTaylor@gmail.com>
2019-04-30 19:54:27 -0700

    Added TradeItem model

---
+++ ReStyle-Frontend/src/app/models/TradeItem.ts
@@ -0,0 +1,10 @@
+// a class for TradeItem objects
+export class TradeItem {
+    itemId:string;
+    userId:string;
+    title:string;
+    size:number;
+    brand:string;
+    description:string;
+    picturePath:string;
+}
