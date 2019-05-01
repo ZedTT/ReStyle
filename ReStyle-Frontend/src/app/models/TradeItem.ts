017a97626ffec7f1c7262e20d151ee97762546b6
ZedTT <ZackThomasTaylor@gmail.com>
2019-04-30 20:09:20 -0700

    clarified the comment for size

--- ReStyle-Frontend/src/app/models/TradeItem.ts
+++ ReStyle-Frontend/src/app/models/TradeItem.ts
@@ -3,7 +3,7 @@ export class TradeItem {
     itemId: string; // a unique id to identify this item
     userId: string; // the id of the user that owns this item
     title: string; // the name of the item
-    size: number; // the size, XS, S, M, L, XL
+    size: number; // the size, 0: XS, 1: S, 2: M, 3: L, 4: XL
     brand: string; // the brand that manufactured this item
     description: string; // the description of the item
     picturePath: string; // a path to the picture that is displayed for this item
