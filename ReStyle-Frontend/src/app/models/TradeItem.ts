c4403d4645637c8abe533ee2f356ef9005fb7d2b
ZedTT <ZackThomasTaylor@gmail.com>
2019-04-30 20:06:47 -0700

    Added comments and whitespace

--- ReStyle-Frontend/src/app/models/TradeItem.ts
+++ ReStyle-Frontend/src/app/models/TradeItem.ts
@@ -1,10 +1,10 @@
 // a class for TradeItem objects
 export class TradeItem {
-    itemId:string;
-    userId:string;
-    title:string;
-    size:number;
-    brand:string;
-    description:string;
-    picturePath:string;
+    itemId: string; // a unique id to identify this item
+    userId: string; // the id of the user that owns this item
+    title: string; // the name of the item
+    size: number; // the size, XS, S, M, L, XL
+    brand: string; // the brand that manufactured this item
+    description: string; // the description of the item
+    picturePath: string; // a path to the picture that is displayed for this item
 }
