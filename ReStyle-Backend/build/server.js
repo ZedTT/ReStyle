42d701f63077b0861afda12bd3c181320af658b6
Kateryna Bozhenko <kateryna.bozhenko@gmail.com>
2019-05-02 11:08:41 -0700

    Made requested changes. Made Node.js working with Angular.

--- ReStyle-Backend/build/server.js
+++ ReStyle-Backend/build/server.js
@@ -6,8 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var fs_1 = __importDefault(require("fs"));
 var app = express_1.default();
+// app.use('/', express.static('../ReStyle-Frontend/dist/ReStyle'));
+app.use('/', express_1.default.static('../ReStyle-Frontend/src'));
 app.get('/', function (req, res) {
-    var doc = fs_1.default.readFileSync("../ReStyle-Frontend/src/app/app.component.html", "utf8");
+    // let doc = fs.readFileSync("../ReStyle-Frontend/dist/ReStyle/index.html", "utf8");
+    var doc = fs_1.default.readFileSync("../ReStyle-Frontend/src/index.html", "utf8");
     res.send(doc);
 });
 var port = 8000;
--- ReStyle-Backend/src/server.ts
+++ ReStyle-Backend/src/server.ts
@@ -3,8 +3,10 @@ import fs from "fs";
 
 const app = express();
 
+app.use('/', express.static('../ReStyle-Frontend/dist/ReStyle'));
+
 app.get('/', function (req, res) {
-    let doc = fs.readFileSync("../ReStyle-Frontend/src/app/app.component.html", "utf8");
+    let doc = fs.readFileSync("../ReStyle-Frontend/dist/ReStyle/index.html", "utf8");
     res.send(doc);
 });
 
