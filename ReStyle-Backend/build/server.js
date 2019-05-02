ce5aa2d1e3cec9e9349a7a36fa56ea7269ec467b
Kateryna Bozhenko <kateryna.bozhenko@gmail.com>
2019-05-02 11:17:40 -0700

    Deleted unnecessary code.

--- ReStyle-Backend/build/server.js
+++ ReStyle-Backend/build/server.js
@@ -4,15 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
-var fs_1 = __importDefault(require("fs"));
 var app = express_1.default();
-// app.use('/', express.static('../ReStyle-Frontend/dist/ReStyle'));
-app.use('/', express_1.default.static('../ReStyle-Frontend/src'));
-app.get('/', function (req, res) {
-    // let doc = fs.readFileSync("../ReStyle-Frontend/dist/ReStyle/index.html", "utf8");
-    var doc = fs_1.default.readFileSync("../ReStyle-Frontend/src/index.html", "utf8");
-    res.send(doc);
-});
+app.use('/', express_1.default.static('../ReStyle-Frontend/dist/ReStyle'));
+// app.get('/', function (req, res) {
+//     let doc = fs.readFileSync("../ReStyle-Frontend/dist/ReStyle/index.html", "utf8");
+//     res.send(doc);
+// });
 var port = 8000;
 app.listen(port, function () {
     console.log("Application is listening on port" + port + "!");
--- ReStyle-Backend/src/server.ts
+++ ReStyle-Backend/src/server.ts
@@ -1,14 +1,8 @@
 import express from 'express';
-import fs from "fs";
 
 const app = express();
 
 app.use('/', express.static('../ReStyle-Frontend/dist/ReStyle'));
-
-app.get('/', function (req, res) {
-    let doc = fs.readFileSync("../ReStyle-Frontend/dist/ReStyle/index.html", "utf8");
-    res.send(doc);
-});
 
 let port = 8000;
 app.listen(port, function () {
