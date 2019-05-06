435158635ffc133e480156f0a7d2cac5e1093257
ZedTT <ZackThomasTaylor@gmail.com>
2019-05-05 22:22:48 -0700

    added getItemsByUser method and made it return static test data

--- ReStyle-Frontend/src/app/services/trade.service.ts
+++ ReStyle-Frontend/src/app/services/trade.service.ts
@@ -6,4 +6,35 @@ import { Injectable } from '@angular/core';
 export class TradeService {
 
   constructor() { }
+
+  getItemsByUser(user: string) {
+    return [
+      {
+        itemId: 'i',
+        userId: user,
+        userPicturePath: '/path',
+        verified: true,
+        rating: 5,
+        title: 'test shirt',
+        size: 3,
+        category: 'shirt',
+        description: 'lorem ipsum dolor sit amet',
+        picturePath: ['/path1', '/path2', '/path3'],
+        bookmarked: false
+      },
+      {
+        itemId: 'k',
+        userId: user,
+        userPicturePath: '/path',
+        verified: false,
+        rating: 4,
+        title: 'test pants',
+        size: 2,
+        category: 'pants',
+        description: 'lorem ipsum dolor sit amet',
+        picturePath: ['/path1', '/path2', '/path3'],
+        bookmarked: true
+      }
+    ];
+  }
 }
