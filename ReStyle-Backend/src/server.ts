f5aa690f01d65b21c5262a0c0e0b807f43312e15
Kateryna Bozhenko <kateryna.bozhenko@gmail.com>
2019-05-05 21:23:40 -0700

    Created a simple test query for our database.

--- ReStyle-Backend/src/server.ts
+++ ReStyle-Backend/src/server.ts
@@ -1,6 +1,26 @@
-import express from 'express';
-
+import express from 'express'; 
+// local dbKey.ts file is required, not pushed to gitHub for security reasons
+import { connectionString } from './dbKey';
 const app = express();
+
+// -----TEST DATABASE CONNECTION-------------------------------------------------------------
+// this is an example used to test connection between node.js server and PostgreSQL database
+const { Pool, Client } = require('pg')
+
+const pool = new Pool({
+  connectionString: connectionString,
+})
+
+const client = new Client({
+  connectionString: connectionString,
+})
+client.connect()
+
+client.query('SELECT t.* FROM dev.restyle_user t LIMIT 501', (err: any, res: any) => {
+  console.log(res.rows)
+  client.end()
+})
+// --------------------------------------------------------------------------------------------
 
 app.use('/', express.static('../ReStyle-Frontend/dist/ReStyle'));
 
