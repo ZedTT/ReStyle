5b5a296bf12ad2aee86dc2baec2313153869faa4
Unknown <catreana@gmail.com>
2019-05-06 07:47:40 -0700

    #62
    includes some dummy data

---
+++ dummyUsers.sql
@@ -0,0 +1,5 @@
+INSERT INTO dev.restyle_user VALUES ('l15CGtMJ5bSnEkRPpYEgyvVWeLt2', 4.8, 'testUser1'),
+('mD7ZT6d9P1bcrBsdQNRGqVaI30m2', 4.2, 'testUser2');
+
+
+
---
+++ testSwap.sql
@@ -0,0 +1,2 @@
+INSERT INTO dev.swap  (userID1, userID2)
+VALUES  ('l15CGtMJ5bSnEkRPpYEgyvVWeLt2','mD7ZT6d9P1bcrBsdQNRGqVaI30m2');
