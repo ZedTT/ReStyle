7833a5a533c3193bc0d1927e67a8d7c576922f4f
ZedTT <ZackThomasTaylor@gmail.com>
2019-05-05 22:07:57 -0700

    Navigate the user to the trade page when they click trade. Log some useful stuff to the console

--- ReStyle-Frontend/src/app/app-routing.module.ts
+++ ReStyle-Frontend/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ItemCardStackComponent } from './components/item-card-stack/item-card-stack.component';
+import { TradePageComponent } from './components/trade-page/trade-page.component';
 
 const routes: Routes = [
-  { path: '', component: ItemCardStackComponent }
+  { path: '', component: ItemCardStackComponent },
+  // { path: '**', redirectTo: '', pathMatch: 'full' },
+  { path: 'trade', component: TradePageComponent }
 ];
 
 @NgModule({
