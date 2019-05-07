339794a8dbe348703125e3a42f919039b27b807d
Kateryna Bozhenko <kateryna.bozhenko@gmail.com>
2019-05-07 10:23:00 -0700

    Fixed types in server.ts

--- ReStyle-Backend/src/server.ts
+++ ReStyle-Backend/src/server.ts
@@ -1,18 +1,18 @@
-import express from 'express'; 
+import express from 'express';
 // local dbKey.ts file is required, not pushed to gitHub for security reasons
 import { connectionString } from './dbKey';
 const app = express();
 
 // -----TEST DATABASE CONNECTION-------------------------------------------------------------
 // this is an example used to test connection between node.js server and PostgreSQL database
-const { Client } = require('pg')
+import { Client } from 'pg'
 
 const client = new Client({
-  connectionString: connectionString,
+    connectionString: connectionString,
 })
 client.connect()
 
-client.query('SELECT t.* FROM dev.restyle_user t LIMIT 501', (err: any, res: any) => {
+client.query('SELECT t.* FROM dev.restyle_user t LIMIT 501', (err, res) => {
   console.log(res.rows)
   client.end()
 })
@@ -21,8 +21,8 @@ client.query('SELECT t.* FROM dev.restyle_user t LIMIT 501', (err: any, res: any
 app.use('/', express.static('../ReStyle-Frontend/dist/ReStyle'));
 
 // this is an example used to test sending simple data to the front end
-app.get('/ajax', function (req: any, res: any) {
-    res.send({'text': 'hello angular'});
+app.get('/ajax', function (req, res) {
+    res.send({ 'text': 'hello angular' });
 });
 
 let port = 8000;
