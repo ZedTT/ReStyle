865f38cbac6bc37817ecfc3b085d50db8936eeed
ZedTT <ZackThomasTaylor@gmail.com>
2019-05-08 13:43:21 -0700

    comment in server.ts

--- ReStyle-Backend/src/server.ts
+++ ReStyle-Backend/src/server.ts
@@ -10,9 +10,9 @@ app.get('/ajax', function (req: any, res: any) {
 /** 
  * Fallback on routes other than '/' because otherwise we get "Cannot get /..." issues.
  * We need to let angular handle the routing
- * ? See https://asknodejs.blogspot.com/2017/06/express-fallback-route-also-used-for.html
+ * ? See https://stackoverflow.com/a/46565926 This has information on the problem that this fixes
  * ? See https://angular.io/guide/deployment#routed-apps-must-fallback-to-indexhtml
- * ? See https://stackoverflow.com/a/46565926
+ * ? See https://asknodejs.blogspot.com/2017/06/express-fallback-route-also-used-for.html
  * ! This must be after every other more specific routing (get and post requests)
  * ! '*' refers to any url that was not already specified above.
  */
