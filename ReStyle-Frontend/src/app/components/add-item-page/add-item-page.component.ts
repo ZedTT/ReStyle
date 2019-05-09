5f0333b3fb231152993ecd7ccc37fba19fbd9c48
Prateek Girdhar <prateek.sgirdhar@gmail.com>
2019-05-09 13:06:38 -0700

    Created functions for user to pick & upload images

--- ReStyle-Frontend/src/app/components/add-item-page/add-item-page.component.ts
+++ ReStyle-Frontend/src/app/components/add-item-page/add-item-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-item-page',
@@ -6,8 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-item-page.component.sass']
 })
 export class AddItemPageComponent implements OnInit {
+  selectedFile: File = null;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  onFileSelected($event) {
+// tslint:disable-next-line: deprecation
+    console.log(event);
+    this.selectedFile = event.target.files[0] as File;
+  }
+
+  onUpload() {
+    const fd = new FormData();
+    fd.append('image', this.selectedFile.name);
+    this.http.post('', fd)
+      .subscribe(res => {
+        console.log(res);
+      });
+  }
 
   ngOnInit() {
   }
