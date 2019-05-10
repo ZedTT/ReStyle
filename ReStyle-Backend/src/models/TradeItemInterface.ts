0a36a5350e47d00f0fdc30dd06824b2de492efe8
Kateryna Bozhenko <kateryna.bozhenko@gmail.com>
2019-05-10 14:46:07 -0700

    Added trade item model (a class with a constrctor and functions) and trade item interface (type inforcement).

---
+++ ReStyle-Backend/src/models/TradeItemInterface.ts
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+
+export interface TradeItemInterface {
+    ownerId: string;
+    description: string;
+    gender: string;
+    size: number;
+    title: string;
+    category: string;
+    photos: string[]
+}
---
+++ ReStyle-Backend/src/models/tradeItemModel.ts
@@ -0,0 +1,23 @@
+import { TradeItemInterface } from './TradeItemInterface';
+
+export class TradeItemModel {
+
+    item: TradeItemInterface
+
+    constructor(item: TradeItemInterface){
+        this.item = item
+    }
+
+    getPropertiesArray(): any[] {
+        return [this.item.ownerId, this.item.description, this.item.gender,
+        this.item.size, this.item.title, this.item.category, this.item.photos]
+    }
+
+    getJSON(): TradeItemInterface{
+        return this.item
+    }
+
+    getSize(): number{
+        return this.item.size
+    }
+}
