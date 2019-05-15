055256c81bcc3e550bd257d87892af51a907fccf
ZedTT <ZackThomasTaylor@gmail.com>
2019-05-15 14:09:28 -0700

    Added comments to AddedItemInterface in the front end

--- ReStyle-Backend/src/models/AddedItemInterface.ts
+++ ReStyle-Backend/src/models/AddedItemInterface.ts
@@ -2,7 +2,8 @@
  * An Item that was just added on the front end.
  * Very similar to TradeItemInterface except that the photos array is an array of type any (Used for form data)
  * In TradeItemInterface, the photos array is an array of strings that represent the url to the images
- * Used in itemRoutes.ts when getting items from the front end.
+ * ? Used in itemRoutes.ts when getting items from the front end.
+ * * There is another model in the front end that is identical to this with the same name
  */
  export interface AddedItemInterface {
     ownerId: string; // The id of the user that just added this item
@@ -11,5 +12,5 @@
     size: number; // The size as a number, 0: XS, 1: S, 2: M, 3: L, 4: XL
     title: string; // The title of the item
     category: string; // The category that the item belongs to (e.g. shirts)
-    photos: any[]; // 
+    photos: any[]; // An array of images form data
 }
