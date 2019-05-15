0ea15470beb78e07c19e92dab0bbe09ee5b4daa8
Haejoon Choi <haejoonchoi@outlook.com>
2019-05-15 15:41:15 -0700

    deleted some dulicate imports

--- ReStyle-Frontend/src/app/app.module.ts
+++ ReStyle-Frontend/src/app/app.module.ts
@@ -4,10 +4,7 @@ import { EventEmitter, NgModule, OnInit, Output } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { FormsModule } from '@angular/forms';
-import { MatButtonToggleModule, MatFormFieldModule, MatInputModule,
-  MatRadioModule, MatSelectModule, MatSliderModule } from '@angular/material'; // tsLint max line 140
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { firebase, firebaseui, FirebaseUIModule } from 'firebaseui-angular';
 import { environment } from '../environments/environment';
