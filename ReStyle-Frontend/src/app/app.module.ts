02a9eef3bed49e93081f0d9b7f63fce58cd75449
Haejoon Choi <haejoonchoi@outlook.com>
2019-05-16 19:17:15 -0700

    Inserted trade requests page button and edited route for it

--- ReStyle-Frontend/src/app/app.module.ts
+++ ReStyle-Frontend/src/app/app.module.ts
@@ -26,6 +26,7 @@ import { NgbdCarouselComponent } from './components/ngbd-carousel/ngbd-carousel.
 import { TradePageComponent } from './components/trade-page/trade-page.component';
 import { ContainerComponent } from './components/container/container.component';
 import { Ng5SliderModule } from 'ng5-slider';
+import { TradeRequestsPageComponent } from './components/trade-requests-page/trade-requests-page.component';
 
 
 const firebaseUiAuthConfig: firebaseui.auth.Config = {
@@ -75,7 +76,8 @@ firebase.initializeApp(environment.firebase);
     LoginCardComponent,
     NgbdCarouselComponent,
     AddItemPageComponent,
-    ContainerComponent
+    ContainerComponent,
+    TradeRequestsPageComponent
   ],
   imports: [
     BrowserModule,
