d36798ad71591264e22b6708525fc89f11bb6da3
ZedTT <ZackThomasTaylor@gmail.com>
2019-05-16 18:28:38 -0700

    Added trade-column component

---
+++ ReStyle-Frontend/src/app/components/trade-column/trade-column.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trade-column',
+  templateUrl: './trade-column.component.html',
+  styleUrls: ['./trade-column.component.sass']
+})
+export class TradeColumnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- ReStyle-Frontend/src/app/models/TradeItem.ts
+++ ReStyle-Frontend/src/app/models/TradeItem.ts
@@ -5,8 +5,8 @@
 export class TradeItem {
     itemId: string; // a unique id to identify this item
     picturePath: string[]; // an array of paths to the pictures that are displayed for this item
-    selected: boolean; // keeps track of if the item is selected for trading or not
     title: string; // the name of the item
     size: number; // the size, 0: XS, 1: S, 2: M, 3: L, 4: XL
     description: string; // the description of the item
+    selected: boolean; // keeps track of if the item is selected for trading or not
 }
