2063651bc40acc212ee883fe68832bfde23f8f35
Kateryna Bozhenko <kateryna.bozhenko@gmail.com>
2019-05-20 11:55:15 -0700

    Moved backend code for /api/userdetails requests to a separate branch

--- ReStyle-Backend/src/controllers/userAccountController.ts
+++ ReStyle-Backend/src/controllers/userAccountController.ts
@@ -1,6 +1,12 @@
 import { query, connect } from '../db/dbInit'
-import { insert_user_with_return, new_user_hide, get_user } from '../db/sql_library'
+import {
+    insert_user_with_return, new_user_hide, get_user, insert_email_contact_details,
+    user_details, update_contact_details_with_return, update_address_details_with_return,
+    update_user_name_and_user_profile_pic,
+    new_address_details
+} from '../db/sql_library'
 import { Response } from 'express';
+import { UserDetailsInterface } from '../models/UserDetailsInterface';
 
 // Initial swap score is hard coded to five
 const initialSwapScore = 5;
@@ -24,7 +30,7 @@ export function getUser(response: Response, uid: string) {
 
 // insert the new user into db
 // ? Picture path is hardcoded to defaultProfilePhotoPath until user changes it in Edit Profile (to be implemented later)
-export function insertNewUser(response: Response, uid: string, userName: string) {
+export function insertNewUser(response: Response, uid: string, userName: string, email: string) {
 
     connect((err, client, done) => {
         if (err) {
@@ -42,12 +48,24 @@ export function insertNewUser(response: Response, uid: string, userName: string)
                         if (error) {
                             done()
                         } else {
-                            response.send(
-                                {
-                                    'message':
-                                        `New user with id: ${uid} and empty hide array for that user were added.`
-                                })
-                            done()
+                            client.query(insert_email_contact_details, [uid, email], (error, result) => { // add user email
+                                if (error) {
+                                    done();
+                                } else {
+                                    client.query(new_address_details, [uid, null, null], (error, result) => {
+                                        if (error) {
+                                            done();
+                                        } else {
+                                            response.send(
+                                                {
+                                                    'message':
+                                                        `New user with id: ${uid} and empty hide array for that user were added.`
+                                                })
+                                            done()
+                                        }
+                                    })
+                                }
+                            })
                         }
                     })
                 } else {
@@ -55,5 +73,77 @@ export function insertNewUser(response: Response, uid: string, userName: string)
                 }
             }
         })
+    })
+}
+
+export function updateUserDetails(response: Response, userDetails: UserDetailsInterface) {
+
+    connect((err, client, done) => {
+        if (err) {
+            console.log("Transaction connection error:", err)
+            response.send({ 'error': err.message })
+            done()
+            return
+        }
+        client.query(update_contact_details_with_return,
+            [userDetails.email, userDetails.phone, userDetails.preferredContact, userDetails.userId],
+            (err, res) => {
+                if (err) {
+                    response.send({ 'error': err.message })
+                    done()
+                }
+                else {
+                    client.query(update_user_name_and_user_profile_pic,
+                        [userDetails.displayname, userDetails.profilePic, userDetails.userId],
+                        (err, res) => {
+                            if (err) {
+                                response.send({ 'error': err.message })
+                                done()
+                            } else {
+                                query(update_address_details_with_return,
+                                    [userDetails.city, userDetails.postalcode, userDetails.userId],
+                                    (err, res) => {
+                                        if (err) {
+                                            response.send({ 'error': err.message })
+                                            done()
+                                        } else {
+                                            if (res.rows.length === 1) {
+                                                response.send({ 'message': "User details were updated successfully." })
+                                            } else {
+                                                response.send({ 'error': `Something went wrong. UID: ${userDetails.userId}` })
+                                            }
+                                            done()
+                                        }
+                                    })
+                            }
+                        })
+                }
+            })
+    })
+}
+
+export function getUserDetails(response: Response, uid: string) {
+    query(user_details, [uid], (error, result) => {
+        if (error) {
+            console.log("Error inside getUserDetails query: ", error)
+            response.send({ error: error.message })
+        } else {
+            if (result.rows.length == 1) {
+                const userRecord = result.rows[0];
+                const userDetailsToSend: UserDetailsInterface = {
+                    userId: uid,
+                    displayname: userRecord.username,
+                    phone: userRecord.phonenumber,
+                    email: userRecord.email,
+                    postalcode: userRecord.postalcode,
+                    city: userRecord.city,
+                    preferredContact: userRecord.preferredmethodofcontact,
+                    profilePic: userRecord.userphotopath
+                }
+                response.send(userDetailsToSend)
+            } else (
+                response.send({ error: `User with uid: ${uid} not found` })
+            )
+        }
     })
 }
--- ReStyle-Backend/src/db/sql_library.js
+++ ReStyle-Backend/src/db/sql_library.js
@@ -11,108 +11,103 @@
 /*
 	Insert user into table
 	with return to get the row back that was just created
-
 	Array of values, fill with three values: 
 	userID character(28) 
 	swapScore Numeric(2,1), swapScore > 0 AND swapScore <= 5
 	userName text
 	userPhotoPath text
-
 	Example:
 	['l15CGtMJ5bSnEkRPpYEgyvVWeLt2', 5, 'Haejoon', null]
 */
-export const insert_user_with_return = 
-"INSERT INTO dev.restyle_user (userID, swapScore, userName, userPhotoPath) "+ 
-"VALUES ($1, $2, $3, $4) RETURNING * "
+export const insert_user_with_return =
+	"INSERT INTO dev.restyle_user (userID, swapScore, userName, userPhotoPath) " +
+	"VALUES ($1, $2, $3, $4) RETURNING * "
 
 /*
 	Insert user into the user table
 	without return of the row that was just created
 	Array of values, fill with three values:
-
 	userID character(28) 
 	swapScore Numeric(2,1), swapScore > 0 AND swapScore <= 5
 	userName text
 	userPhotoPath text 
-
 	Example:
 	['l15CGtMJ5bSnEkRPpYEgyvVWeLt2',1, 'Haejoon', null]
 */
-export const insert_user_no_return = 
-"INSERT INTO dev.restyle_user (userID, userName, userPhotoPath) "+ 
-"VALUES ($1, $2, $3) "
+export const insert_user_no_return =
+	"INSERT INTO dev.restyle_user (userID, userName, userPhotoPath) " +
+	"VALUES ($1, $2, $3) "
 
 /*
 	Get user based on user id from login 
 	userID character(28)
-
 	Example:
 	['l15CGtMJ5bSnEkRPpYEgyvVWeLt2']
-
 */
-export const get_user = 
-"SELECT * FROM dev.restyle_user "+
-"WHERE userID = $1"
+export const get_user =
+	"SELECT * FROM dev.restyle_user " +
+	"WHERE userID = $1"
 
 /*
 	Get user rating based on the userID 
 	userID character(28)
-
 	Example:
 	['l15CGtMJ5bSnEkRPpYEgyvVWeLt2']
 */
 export const get_user_rating =
-"SELECT AVG(rating) FROM dev.rating " +
-"WHERE userID = $1"
+	"SELECT AVG(rating) FROM dev.rating " +
+	"WHERE userID = $1"
 
 /*
-
 	Get the the data from both the item table 
 	and the user table
-
 	userID character(28)
-
 	Example:
 	[userID]
 	['l15CGtMJ5bSnEkRPpYEgyvVWeLt2']
 */
 export const get_user_item_data =
-"SELECT i.*, u.swapScore, u.userName, u.userPhotoPath " +
-"FROM dev.item AS i " +
-"INNER JOIN dev.restyle_user AS u " +
-"ON i.userID = u.userID " +
-"WHERE i.userID != $1 " +
-"AND i.swapID IS NULL "
+	"SELECT i.*, u.swapScore, u.userName, u.userPhotoPath " +
+	"FROM dev.item AS i " +
+	"INNER JOIN dev.restyle_user AS u " +
+	"ON i.userID = u.userID " +
+	"WHERE i.userID != $1 " +
+	"AND i.swapID IS NULL "
 
 /*
 	Update the user's profile picture 
 	
 	userID character(28)
 	userPhotoPath text
-
 	Example:
 	[profilePicPhotoPath, userID]
 	['nathangreen_avatar.jpg', 'rCjzKDG6rjUwjj6I5BepsLbvgPr1']
 */
 
 export const update_user_profile_pic =
-"UPDATE dev.restyle_user AS u " +
-"SET u.userPhotoPath = $1 "
+	"UPDATE dev.restyle_user AS u " +
+	"SET u.userPhotoPath = $1 "
 "WHERE userID = $2 "
 
 /*
 	Update user name 
-
 	userID character(28)
-
 	Example:
 	[userName, userID]
 	['newUserName', 'rCjzKDG6rjUwjj6I5BepsLbvgPr1']
 */
-export const update_user_name = 
-"UPDATE dev.restyle_user AS u " +
-"SET u.userName = $1 " +
-"WHERE u.userName = $2 "
+export const update_user_name =
+	"UPDATE dev.restyle_user AS u " +
+	"SET u.userName = $1 " +
+	"WHERE u.userID = $2 " // fixed typo, was userName, needed to be userID
+
+/**
+ * A query to update both user name and user profile picture at one go.
+ */
+export const update_user_name_and_user_profile_pic =
+	"UPDATE dev.restyle_user " +
+	"SET userName = $1, userPhotoPath = $2 " +
+	"WHERE userID = $3 "
 
 /*
 --------------------------------------------------Item Queries----------------------------------
@@ -128,13 +123,12 @@ export const update_user_name =
 	gender text, Male, Female or Unisex
 	size integer, (size > 0 AND size <= 4)
 	title, category, photoPaths text
-
 	Example:
 	['l15CGtMJ5bSnEkRPpYEgyvVWeLt2', null , 'description', 'Female', 1, 'title', 'category', ['path1','path2','path3]]
 */
 export const insert_item_with_return =
-"INSERT INTO dev.item (userID, description, gender, size, title, category, photoPaths) " +
-"VALUES ($1, $2, $3, $4, $5, $6, $7::text[]) RETURNING * "
+	"INSERT INTO dev.item (userID, description, gender, size, title, category, photoPaths) " +
+	"VALUES ($1, $2, $3, $4, $5, $6, $7::text[]) RETURNING * "
 
 /*
 	Insert item into item table without return 
@@ -143,13 +137,12 @@ export const insert_item_with_return =
 	gender text, Male, Female or Unisex
 	size integer, (size > 0 AND size <= 4)
 	title, category, photoPaths text
-
 	Example:
 	['l15CGtMJ5bSnEkRPpYEgyvVWeLt2', 'description', 'Female', 1, 'title', 'category', ['path1','path2','path3]]
 */
 export const insert_item_no_return =
-"INSERT INTO dev.item (userID, description, gender, size, title, category, photoPaths) " +
-"VALUES ($1, $2, $3, $4, $5, $6, $7::text[]) "
+	"INSERT INTO dev.item (userID, description, gender, size, title, category, photoPaths) " +
+	"VALUES ($1, $2, $3, $4, $5, $6, $7::text[]) "
 
 
 /*
@@ -157,33 +150,24 @@ export const insert_item_no_return =
 	This is for the user viewing their inventory
 	Do not want the user to see items they have already
 	swapped 
-
 	Example:
 	[userID]
 	['l15CGtMJ5bSnEkRPpYEgyvVWeLt2']
 */
 export const get_user_item =
-"SELECT * FROM dev.item " +
-"WHERE userID = $1 AND swapID IS NULL "
-
-/**
- * Get all the items owned by a user that are eligible for trading, i.e. don't have swapID
- */
-export const get_user_items_to_trade =
-"SELECT * FROM dev.item " +
-"WHERE userID = $1 AND swapID IS NULL "
+	"SELECT * FROM dev.item " +
+	"WHERE userID = $1 AND swapID IS NULL "
 
 /*
 	Get item(s) to display for a user that does not 
 	include their own items
-
 	Example:
 	[userID]
 	['l15CGtMJ5bSnEkRPpYEgyvVWeLt2']
 */
 export const display_item =
-"SELECT * FROM dev.item " +
-"WHERE userID != $1 AND swapID IS NULL "
+	"SELECT * FROM dev.item " +
+	"WHERE userID != $1 AND swapID IS NULL "
 
 /*
 	Insert the item's photoPath array into the item table
@@ -193,25 +177,21 @@ export const display_item =
 	[[photoPath1, photoPath2],itemID]
 */
 export const add_photoPath_to_item =
-"UPDATE dev.item SET photoPaths = array_cat(photoPaths, $1::text[]) " +
-"WHERE itemID = $2 "
+	"UPDATE dev.item SET photoPaths = array_cat(photoPaths, $1::text[]) " +
+	"WHERE itemID = $2 "
 
 /*
 	Get photos in array for an item
-
 	itemID as an integer
-
 	Example:
 	[itemID]
-
 */
 export const get_photos =
-"SELECT photoPaths FROM dev.item " +
-"WHERE itemID = $1 "
+	"SELECT photoPaths FROM dev.item " +
+	"WHERE itemID = $1 "
 
 /*
 	Display items with pagination
-
 	userID character(28)
 	Limit: no more than that many rows will 
 			be returned (but possibly less, if the query itself yields less rows)
@@ -223,24 +203,22 @@ export const get_photos =
 	third page: [userID, 5, 10]
 */
 export const display_items_paginated =
-"SELECT * FROM dev.item " +
-"WHERE userID != $1 AND swapID IS NULL " +
-"ORDER BY itemID " + 
-"LIMIT $2 OFFSET $3 "
+	"SELECT * FROM dev.item " +
+	"WHERE userID != $1 AND swapID IS NULL " +
+	"ORDER BY itemID " +
+	"LIMIT $2 OFFSET $3 "
 
 /*
 	Update item(s) with swapID
-
 	swapID as an integer
 	itemID as an integer
-
 	Example:
 	[swapID, [itemID1, itemID2]
 	[1, [1, 2]]
 */
 export const item_add_swapID =
-"UPDATE dev.item SET swapID = $1 " +
-"WHERE itemID = Any($2::INT[]) "
+	"UPDATE dev.item SET swapID = $1 " +
+	"WHERE itemID = Any($2::INT[]) "
 
 /*
 --------------------------------------------------Hide Queries-------------------------------
@@ -254,64 +232,54 @@ export const item_add_swapID =
 	
 	userID character(28)
 	items should be initialized as an empty array
-
 	Example:
 	[userID, emptyArray]
 	['15CGtMJ5bSnEkRPpYEgyvVWeLt2', []]
-
-
 	note: create an empty array when user is first created/inserted
 	into the user table
 */
 export const new_user_hide =
-"INSERT INTO dev.hide (userID, items) " + 
-"VALUES ($1, '{}') " 
+	"INSERT INTO dev.hide (userID, items) " +
+	"VALUES ($1, '{}') "
 
 /*
 	Get hide list for a specific user based on the userID
 	userID character(28)
-
 	Example:
 	[userID]
 	['15CGtMJ5bSnEkRPpYEgyvVWeLt2']
 */
 export const get_hide_list =
-"SELECT items FROM dev.hide " +
-"WHERE userID = $1 "
+	"SELECT items FROM dev.hide " +
+	"WHERE userID = $1 "
 
 /*
 	Add item to hide table for a specific user
-
 	userID character(28)
 	itemID integer
-
 	Note array_append: append an element to the end of an array
 	Note: items = array_append(items, $1) is the same as items = items + 1
-
 	Example:
 	[itemID, userID]
 	[23,'15CGtMJ5bSnEkRPpYEgyvVWeLt2']
 */
 export const add_hide =
-"UPDATE dev.hide SET items = array_append(items, $1) " +
-"WHERE userID = $2 "
+	"UPDATE dev.hide SET items = array_append(items, $1) " +
+	"WHERE userID = $2 "
 
 
 /*
 	Remove an item from the hide table for a specific user
-
 	userID character(28)
 	itemID integer
-
 	Note array_append: append an element to the end of an array
-
 	Example:
 	[itemID, userID]
 	[23,'15CGtMJ5bSnEkRPpYEgyvVWeLt2']
 */
 export const remove_hide =
-"UPDATE dev.hide SET items = array_remove(items, $1) " +
-"WHERE userID = $2 "
+	"UPDATE dev.hide SET items = array_remove(items, $1) " +
+	"WHERE userID = $2 "
 
 /*
 --------------------------------------------------Trade Request Queries-------------------------------
@@ -319,7 +287,6 @@ export const remove_hide =
 
 /*
 	Create new trade request with return
-
 	requester_userID1 character(28)
 	notfied_userID2 character(28)
 	requester_itemArray1 an array of integers
@@ -327,16 +294,14 @@ export const remove_hide =
 	
 	Example:
 	[userID, userID, [itemID1, itemID2], [itemID1,itemID2]]
-
 */
 export const new_trade_request_with_return =
-"INSERT INTO dev.trade_request " +
-"(requester_userID1, notified_userID2, requester_itemArray1, notified_itemArray2) " +
-"VALUES ($1, $2, $3::integer[], $4::integer[]) RETURNING * "
+	"INSERT INTO dev.trade_request " +
+	"(requester_userID1, notified_userID2, requester_itemArray1, notified_itemArray2) " +
+	"VALUES ($1, $2, $3::integer[], $4::integer[]) RETURNING * "
 
 /*
 	Create new trade request with no return
-
 	requester_userID1 character(28)
 	notfied_userID2 character(28)
 	requester_itemArray1 an array of integers
@@ -344,26 +309,22 @@ export const new_trade_request_with_return =
 	
 	Example:
 	[userID, userID, [item1, item2], [item1,item2]]
-
 */
 export const new_trade_request_no_return =
-"INSERT INTO dev.trade_request " +
-"(requester_userID1, notified_userID2, requester_itemArray1, notified_itemArray2) " +
-"VALUES ($1, $2, $3::integer[], $4::integer[]) "
+	"INSERT INTO dev.trade_request " +
+	"(requester_userID1, notified_userID2, requester_itemArray1, notified_itemArray2) " +
+	"VALUES ($1, $2, $3::integer[], $4::integer[]) "
 
 /*
 	Update status for trade request
-
 	tradeRequestID integer
-
 	Example:
 	[status, tradeRequestID]
 	[Accept/Reject, 1]
-
 */
 export const status_update_trade_request =
-"UPDATE dev.trade_request SET status = $1 " +
-"WHERE tradeRequestID = $2 "
+	"UPDATE dev.trade_request SET status = $1 " +
+	"WHERE tradeRequestID = $2 "
 
 /*
 --------------------------------------------------Swap Queries-------------------------------
@@ -372,32 +333,28 @@ export const status_update_trade_request =
 /*
 	Add swap to swap table after a trade request has been approved
 	with return
-
 	userID of requeter: character (28)
 	userID of requestee: character (28)
-
 	Example:
 	[userID1, userID2]
 	['l15CGtMJ5bSnEkRPpYEgyvVWeLt2', 'mD7ZT6d9P1bcrBsdQNRGqVaI30m2']
 */
 export const add_swap_with_return =
-"INSERT INTO dev.swap " +
-"VALUES ($1, $2) RETURNING * "
+	"INSERT INTO dev.swap " +
+	"VALUES ($1, $2) RETURNING * "
 
 /*
 	Add swap to swap table after a trade request has been approved
 	no return
-
 	userID of requeter: character (28)
 	userID of requestee: character (28)
-
 	Example:
 	[userID1, userID2]
 	['l15CGtMJ5bSnEkRPpYEgyvVWeLt2', 'mD7ZT6d9P1bcrBsdQNRGqVaI30m2']
 */
 export const add_swap_no_return =
-"INSERT INTO dev.swap " +
-"VALUES ($1, $2) "
+	"INSERT INTO dev.swap " +
+	"VALUES ($1, $2) "
 
 /*
 --------------------------------------------------Contact Details Queries-------------------------------
@@ -412,11 +369,10 @@ export const add_swap_no_return =
 	Example:
 	[userID, email, phoneNumber, preferredMethodOfContact]
 	['l15CGtMJ5bSnEkRPpYEgyvVWeLt2', email, phoneNumber, 'Phone'/'Email']
-
 */
 export const new_contact_details_no_return =
-"INSERT INTO dev.contact_details  (userID, email, phoneNumber, preferredMethodOfContact) " +
-"VALUES ($1, $2, $3, $4) "
+	"INSERT INTO dev.contact_details  (userID, email, phoneNumber, preferredMethodOfContact) " +
+	"VALUES ($1, $2, $3, $4) "
 
 /*
 	Insert contact details into contact details table with return 
@@ -427,43 +383,39 @@ export const new_contact_details_no_return =
 	Example:
 	[email, phoneNumber, preferredMethodOfContact, userID]
 	[email, phoneNumber, 'Phone'/'Email','l15CGtMJ5bSnEkRPpYEgyvVWeLt2']
-
 */
 export const update_contact_details_with_return =
-"UPDATE dev.contact_details AS c " +
-"SET c.email = $1, c.phoneNumber = $2, c.preferredMethodOfContact = $3 " +
-"WHERE userID = $4 RETURNING * "
+	"UPDATE dev.contact_details " +
+	"SET email = $1, phoneNumber = $2, preferredMethodOfContact = $3 " +
+	"WHERE userID = $4 RETURNING * "
 
 /*
 	Insert email into the contact details table
-
 	userID character(28)
 	email text
-
 	Example:
 	[userID, email]
 	['l15CGtMJ5bSnEkRPpYEgyvVWeLt2', 'email']
 */
-export const insert_email_contact_details = 
-"INSERT INTO dev.contact_details (userID, email) " +
-"VALUES ($1, $2) "
+export const insert_email_contact_details =
+	"INSERT INTO dev.contact_details (userID, email) " +
+	"VALUES ($1, $2) "
 
 /*
 	Retrieve data needed for user details interface
-
 	Example:
 	[userID]
 	['l15CGtMJ5bSnEkRPpYEgyvVWeLt2']
 */
 export const user_details =
-"SELECT u.userName, u.userPhotoPath, c.phoneNumber, " +
-"c.email, a.postalCode, a.city, c.preferredMethodOfContact " +
-"FROM dev.restyle_user AS u " +
-"INNER JOIN dev.contact_details AS c " +
-"ON u.userID = c.userID " +
-"INNER JOIN dev.address AS a " +
-"ON a.userID = u.userID " +
-"WHERE u.userID = $1 "
+	"SELECT u.userName, u.userPhotoPath, c.phoneNumber, " +
+	"c.email, a.postalCode, a.city, c.preferredMethodOfContact " +
+	"FROM dev.restyle_user AS u " +
+	"INNER JOIN dev.contact_details AS c " +
+	"ON u.userID = c.userID " +
+	"INNER JOIN dev.address AS a " +
+	"ON a.userID = u.userID " +
+	"WHERE u.userID = $1 "
 
 
 /*
@@ -476,8 +428,15 @@ export const user_details =
 	Example:
 	[userID, city, postalCode]
 	['l15CGtMJ5bSnEkRPpYEgyvVWeLt2', email]
-
 */
-export const new_address_details = 
-"INSERT INTO dev.address (userID, city, postalCode) " +
-"VALUES ($1, $2 , $3) "
+export const new_address_details =
+	"INSERT INTO dev.address (userID, city, postalCode) " +
+	"VALUES ($1, $2 , $3) "
+
+/**
+ * A query to update user address details when user is editing their profile.
+ */
+export const update_address_details_with_return =
+	"UPDATE dev.address " +
+	"SET city = $1, postalCode = $2 " +
+	"WHERE userID = $3 RETURNING * "
