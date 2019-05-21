236220ff131b1082bae31bd3f0ebeeb54f79fedc
ZedTT <ZackThomasTaylor@gmail.com>
2019-05-21 13:02:34 -0700

    Added 404 page

--- ReStyle-Frontend/src/app/app-routing.module.ts
+++ ReStyle-Frontend/src/app/app-routing.module.ts
@@ -7,16 +7,18 @@ import { AddItemPageComponent } from './components/add-item-page/add-item-page.c
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { TradeRequestsPageComponent } from './components/trade-requests-page/trade-requests-page.component';
 import { AboutUsPageComponent } from './components/about-us-page/about-us-page.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
-  // { path: '**', redirectTo: '', pathMatch: 'full' },
   { path: 'trade', component: TradePageComponent },
   { path: 'additem', component: AddItemPageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'userprofile', component: UserProfileComponent },
   { path: 'traderequests', component: TradeRequestsPageComponent },
-  { path: 'aboutus', component: AboutUsPageComponent}
+  { path: 'aboutus', component: AboutUsPageComponent},
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '/404', pathMatch: 'full' }
 ];
 
 @NgModule({
