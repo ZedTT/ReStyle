fc4b30dd0eff63e0ae4c853889c5ac62980029a4
ZedTT <47646286+ZedTT@users.noreply.github.com>
2019-09-24 19:25:48 -0700

    Added numberOfIncomingTradeRequests

--- ReStyle-Frontend/src/app/components/header/header.component.ts
+++ ReStyle-Frontend/src/app/components/header/header.component.ts
@@ -10,6 +10,7 @@ export class HeaderComponent implements OnInit {
   @Output() easterEggOutput: EventEmitter<boolean> = new EventEmitter();
   easterEggEnabled: boolean;
   easterEggClickCount = 0;
+  numberOfIncomingTradeRequests: number; // TODO: consider rename? vs code f2
   incomingTradeRequestExists: boolean;
 
   constructor(private tradeRequestService: TradeRequestService, private ngZone: NgZone) {
@@ -32,11 +33,16 @@ export class HeaderComponent implements OnInit {
     return classes;
   }
 
+  // TODO: Check this again whenever we interact with a trade request
   checkForIncomingTradeRequests() {
     firebase.auth().onAuthStateChanged(user => {
       this.ngZone.run(() => { // make firebase run in ng zone
         this.tradeRequestService.getTradeRequestsByUser(user ? user.uid : null).subscribe(temp => {
-          this.incomingTradeRequestExists = (temp) ? (temp.length >= 1) : false;
+          this.numberOfIncomingTradeRequests = (temp) ? (temp.length) : 0;
+          // could probably just make the ng if more complicated in the html instead, 
+          // but that sounds like work and we are in CST right now so this stays. 
+          // Our tech debt must grow.
+          this.incomingTradeRequestExists = (this.numberOfIncomingTradeRequests >= 1);
         });
       });
     });
